a5fda7b97f722d5f7cac7d70032f35ec5a93ab75
Leif <leif.s.goodwin@gmail.com>
2020-05-24 12:40:53 +0100

    Added a missing file

---
+++ WpfOpenControls/DockManager/ToolHeaderStyle.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfOpenControls.DockManager
+{
+    public class ToolHeaderStyle
+    {
+        public CornerRadius CornerRadius { get; set; }
+        public Brush BorderBrush { get; set; }
+        public Thickness BorderThickness { get; set; }
+        public Brush Background { get; set; }
+    }
+}
