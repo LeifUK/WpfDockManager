1c3f50d5a769ed31cd126fa20f4df41446f7d9a5
Leif <leif.s.goodwin@gmail.com>
2020-05-29 08:57:37 +0100

    Don't need these files

---
+++ ExampleDockManagerViews/ViewModel/DocumentViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ExampleDockManagerViews.ViewModel
+{
+    public class DocumentViewModel : DummyViewModel
+    {
+        public override string LongTitle
+        {
+            get
+            {
+                return Title + " : " + URL;
+            }
+        }
+    }
+}
---
+++ ExampleDockManagerViews/ViewModel/ToolViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleDockManagerViews.ViewModel
+{
+    public class ToolViewModel : DummyViewModel
+    {
+        public override string LongTitle
+        {
+            get
+            {
+                return Title;
+            }
+        }
+    }
+}
